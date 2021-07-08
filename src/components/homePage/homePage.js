61b8479888f5503e6f4ced0a0aa7ede42605c249
Julio Lopez <julioj.lopeza@gmail.com>
2021-07-09 00:41:10 +0200

    Added the homepage component

--- src/components/homePage/homePage.js
+++ src/components/homePage/homePage.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function homePage() {
+import Navigation from '../navigation/Navigation'
+import Banner from '../banner/Banner'
+import About from '../about/About'
+import Footer from '../footer/Footer'
+import Contact from '../contact/Contact'
+import Projects from '../projects/Projects'
+import ThemeSwitch from '../themeSwitch/ThemeSwitch'
+
+export default function HomePage() {
+
+  const [ switchValue, setSwitchValue ] = useState(false)
+
+  useEffect(() => {
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+    if (currentTheme) {
+      document.documentElement.setAttribute('data-theme', currentTheme);
+      if (currentTheme === 'dark') {
+        setSwitchValue(true)
+      }
+    }
+  }, [])
+
+  const handleSwitch = e => {
+    setSwitchValue(e.target.checked)
+    if (e.target.checked) {
+      localStorage.setItem('theme', 'dark');
+      document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', 'light');
+    }
+  }
+
   return (
     <div>
-      "homePage"
+      <ThemeSwitch handleSwitch={handleSwitch} switchValue={switchValue} />
+      <Navigation />
+      <Banner />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer dark={switchValue} />
     </div>
   )
 }
