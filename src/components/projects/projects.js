3a3f083c7c97315805b117f05dd65fd5f6ccdb5c
Julio Lopez <julioj.lopeza@gmail.com>
2021-07-09 12:57:09 +0200

    Added project component to show main project in the homepage

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
-export default function projects() {
+import sanityClient from '../../client'
+import ProjectCard from './ProjectCard'
+import './projects.css'
+
+export default function Projects() {
+  const [ projects, setProjects ] = useState([])
+  console.log(projects)
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "project" && main == true] | order(publishedAt) {
+          title,
+          slug,
+          description,
+          webpage,
+          github,
+          githubSecondary,
+          playstore,
+          tags,
+          publishedAt,
+          projectType,
+          mainImage{
+            asset->{
+              _id,
+              url
+            }
+          }
+        }`
+      )
+      .then((data) => setProjects(data))
+      .catch(console.error);
+  }, []);
+
   return (
-    <div>
-      "projects"
-    </div>
+    <section id="projects" className="projects">
+      <h2 className="projects__title">PROJECTS</h2>
+      <div className="projects__container">
+        {projects.map(project =>
+          <ProjectCard project={project}/>
+        )}
+        <Link to="/projects">sell all the projects</Link>
+      </div>
+    </section>
   )
 }
