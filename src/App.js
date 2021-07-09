c3c9f9b824fc438f06583382a0ffba8dd0be3740
Julio Lopez <julioj.lopeza@gmail.com>
2021-07-09 12:54:31 +0200

    Added the switch theme to the main of the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import Footer from './components/footer/Footer';
 
 import HomePage from './components/homePage/HomePage'
 import Posts from './components/posts/Posts'
-import Projects from './components/projects/Projects'
+import AllProjects from './components/projects/AllProjects';
 import SinglePost from './components/singlePost/SinglePost'
 import SingleProject from './components/singleProject/SingleProject'
 
 function App() {
+  const [ switchValue, setSwitchValue ] = useState(false)
+
+  useEffect(() => {
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+    if (currentTheme) {
+      document.documentElement.setAttribute('data-theme', currentTheme);
+      if (currentTheme === 'dark') {
+        setSwitchValue(true)
+      }
+    }
+  }, [])
+
+  const handleSwitch = e => {
+    setSwitchValue(e.target.checked)
+    if (e.target.checked) {
+      localStorage.setItem('theme', 'dark');
+      document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', 'light');
+    }
+  }
+
   return (
     <Router>
       <Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route exact path='/projects' component={Projects}/>
+        <Route exact path='/' render={() => <HomePage handleSwitch={handleSwitch} switchValue={switchValue} />}/>
+        <Route exact path='/projects' component={AllProjects}/>
         <Route path='/projects/:id' component={SingleProject}/>
         <Route exact path='/posts' component={Posts}/>
         <Route path='/posts/:id' component={SinglePost}/>
       </Switch>
+      <Footer dark={switchValue} />
     </Router>
   );
 }
