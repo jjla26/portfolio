f6a6f89c84b921a87cd395369cfd99bd1a086add
Julio Lopez <julioj.lopeza@gmail.com>
2021-07-09 12:53:54 +0200

    Added react router to the navigation header

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,25 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 import './navigation.css'
 
 export default function Navigation() {
+  const { hash } = useLocation()
+
+  useEffect(() => {
+    if(hash === ''){
+      window.scrollTo(0, 0);
+    }else{
+      setTimeout(() => {
+        const id = hash.replace('#', '');
+        const element = document.getElementById(id);
+        if (element) {
+          element.scrollIntoView();
+        }
+      }, 500)
+    }
+  }, [hash])
+
   return (
     <header className="header">
-      <a className="header__title" href="index.html"><h1>JULIO J. LOPEZ A.</h1></a>
+      <Link className="header__title" to="/"><h1>JULIO J. LOPEZ A.</h1></Link>
       <h3>Full Stack Web Developer</h3>
       <nav>
         <ul className="navigation">
           <li role="presentation" className="navigation-list__item">
-              <a href="#profile" >Home</a>
+              <Link to="/" >Home</Link>
           </li>
           <li role="presentation" className="navigation-list__item">
-              <a href="#about" >About</a>
+              <Link to="/#about" >About</Link>
           </li>
           <li role="presentation" className="navigation-list__item">
-              <a href="#project" >Work</a>
+              <Link to="/projects" >Work</Link>
           </li>
           <li role="presentation" className="navigation-list__item">
-              <a href="#contact" >Contact</a>
+              <Link to="/#contact" >Contact</Link>
           </li>
         </ul>
       </nav>
