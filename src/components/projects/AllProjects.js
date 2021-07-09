a07624ca8b9aee17216672c3d31f7612394ea95e
Julio Lopez <julioj.lopeza@gmail.com>
2021-07-09 12:56:07 +0200

    Added an AllProjects component to show all the projects

---
+++ src/components/projects/AllProjects.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react'
+
+import Navigation from '../navigation/Navigation'
+import sanityClient from '../../client'
+import ProjectCard from './ProjectCard'
+import './projects.css'
+
+export default function AllProjects() {
+  const [ projects, setProjects ] = useState([])
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "project"] | order(publishedAt) {
+          title,
+          slug,
+          description,
+          webpage,
+          github,
+          githubSecondary,
+          playstore,
+          tags,
+          publishedAt,
+          projectType,
+          mainImage{
+            asset->{
+              _id,
+              url
+            }
+          }
+        }`
+      )
+      .then((data) => setProjects(data))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <>
+      <Navigation />
+      <section>
+        <h2 className="projects__title">PROJECTS</h2>
+        <div className="projects__container">
+          {projects.map(project =>
+            <ProjectCard key={project.slug.current} project={project}/>
+          )}
+        </div>
+      </section>
+    </>
+  )
+}
