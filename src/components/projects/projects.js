479995ccfd246507344d7d6a8472dc5160bbd0ac
Julio Lopez <julioj.lopeza@gmail.com>
2021-07-15 00:14:49 +0200

    Added body to show study case

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -20,6 +20,7 @@ export default function Projects() {
           githubSecondary,
           playstore,
           tags,
+          body,
           publishedAt,
           projectType,
           mainImage{
