76b7a493d9032b7f348d05b6a14cc27ebf46ab7c
Julio Lopez <julioj.lopeza@gmail.com>
2021-07-14 17:46:19 +0200

    Created a single project view for the study cases

--- src/components/singleProject/singleProject.js
+++ src/components/singleProject/singleProject.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import BlockContent from '@sanity/block-content-to-react'
 
-export default function singleProject() {
+import sanityClient from '../../client'
+import Navigation from '../navigation/Navigation'
+import './singleproject.css'
+
+
+export default function SingleProject() {
+  const [ project, setProject ] = useState(null)
+  const { id } = useParams()
+  console.log(project, id)
+
+  useEffect(() => {
+    sanityClient.fetch(
+      `*[slug.current == "${id}"]{
+        title,
+        slug,
+        tags,
+        publishedAt,
+        projectType,
+        body
+      }`
+    )
+    .then(data => setProject(data[0]))
+    .catch(error => error )
+  
+  }, [id])
+
+  if(!project) return <div></div>
+
   return (
-    <div>
-      "singleProject"
-    </div>
+    <>
+    <Navigation />
+    <section class="single-project">
+      <div class="single-content">
+        <h1>{project.title}</h1>
+        <div>
+          <span><strong>Published at:</strong> {new Date(project.publishedAt).toLocaleDateString()} </span>
+          <span><strong>Project Type:</strong> {project.projectType} </span><br />
+        </div>
+        <BlockContent 
+          blocks={project.body}
+          projectId='dqkk8vgs'
+          dataset='production'
+        />
+      </div>
+    </section>
+    </>
   )
 }
