3d37263f8c72c866edff737a8cdc10c892dea1ad
Julio Lopez <julioj.lopeza@gmail.com>
2021-07-14 19:31:18 +0200

    Added hashrouting to work with ghpages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import Footer from './components/footer/Footer';
 
